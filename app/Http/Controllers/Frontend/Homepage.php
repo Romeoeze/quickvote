065ec5a97418ea0be25dfd5e6a563762b70b3309
Romeoeze <46187562+Romeoeze@users.noreply.github.com>
2022-09-03 21:21:14 +0100

    menu modifications

--- app/Http/Controllers/Frontend/Homepage.php
+++ app/Http/Controllers/Frontend/Homepage.php
@@ -37,7 +37,7 @@ class Homepage extends Controller
 
     public function indexnew(){
 
-        $contests = Contest::OrderBy('created_at', 'DESC')->with('vendor')->get()->shuffle()->all();
+        $contests = Contest::OrderBy('created_at', 'DESC')->with('vendor')->all();
        $sliderData = Slider::FindorFail(1);
        $brands = Logo::latest()->get();
         $contestmultis = Multicontest::OrderBy('created_at', 'DESC')->with('vendor')->get()->shuffle()->all();
